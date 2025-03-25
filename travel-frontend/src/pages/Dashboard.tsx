6ba1812740b01094d165cd20610a1a8aeb9b67b4
ducnhanit <buiducnhan.it@gmail.com>
2025-03-25 14:13:46 +0700

    feat: initial frontend project and setup tailwindcss, mui, redux, react-router

---
+++ travel-frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="flex  justify-center p-10">
+            <h1 className="text-3xl font-bold text-gray-800">Admin Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ travel-frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-3xl font-bold text-blue-600">Welcome to Travel App</h1>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
